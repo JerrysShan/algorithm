e4be3ac3d02657a98b5918ae8391aba4b2ecf1c3
JerrysShan <1670303003@qq.com>
2017-08-01 22:01:13 +0800

    修改

--- sort/quick_sort.go
+++ sort/quick_sort.go
@@ -1,5 +1,34 @@
 package main
 
-func quickSort() {
+import "fmt"
 
+func main() {
+	arr := []int{1, 23, 4, 5, 2, 9, 10}
+	quickSort(arr, 0, len(arr)-1)
+	for _, item := range arr {
+		fmt.Println(item)
+	}
+}
+
+func quickSort(arr []int, low, high int) {
+	if low > high {
+		return
+	}
+	key := arr[low]
+	i := low
+	j := high
+	for i < j {
+		for arr[j] >= key && i < j {
+			j--
+		}
+		for arr[i] <= key && i < j {
+			i++
+		}
+		if i < j {
+			arr[j], arr[i] = arr[i], arr[j]
+		}
+	}
+	arr[low], arr[i] = arr[i], arr[low]
+	quickSort(arr, low, i-1)
+	quickSort(arr, i+1, high)
 }
