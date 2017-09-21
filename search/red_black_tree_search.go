279db3baf4f0478b9073d5c50868adb3d1966e7f
JerrysShan <1670303003@qq.com>
2017-09-21 23:03:37 +0800

    修改

--- search/red_black_tree_search.go
+++ search/red_black_tree_search.go
@@ -37,9 +37,9 @@ func (rb *RedBlackBST) rotateRight(h *Node) *Node {
 }
 
 func (rb *RedBlackBST) filpColors(h *Node) {
-   h.left.color=BLACK
-   h.right.color=BLACK
-   h.color=RED
+	h.left.color = BLACK
+	h.right.color = BLACK
+	h.color = RED
 }
 
 func (rb *RedBlackBST) putRoot(key int) {
@@ -52,7 +52,7 @@ func (rb *RedBlackBST) put(h *Node, key int) *Node {
 		return &Node{
 			data:  key,
 			color: RED,
-	        }
+		}
 	}
 	if key < h.data {
 		h.left = rb.put(h.left, key)
@@ -70,5 +70,54 @@ func (rb *RedBlackBST) put(h *Node, key int) *Node {
 	if rb.isRed(h.left) && rb.isRed(h.right) {
 		rb.filpColors(h)
 	}
+	return h
+}
+
+func (rb *RedBlackBST) DeleteMin() {
+	if !rb.isRed(rb.root) && !rb.isRed(rb.root) {
+		rb.root.color = RED
+	}
+	rb.root = rb.deleteMin(rb.root)
+	rb.root.color = BLACK
+}
+
+func (rb *RedBlackBST) deleteMin(node *Node) *Node {
+	if node.left == nil {
+		return nil
+	}
+	return nil
+}
+
+func (rb *RedBlackBST) moveRedLeft(node *Node) *Node {
+	rb.filpColors(node)
+	if rb.isRed(node.right.left) {
+		node.right = rb.rotateRight(node.right)
+		node = rb.rotateLeft(node)
+		rb.filpColors(node)
+	}
+	return node
+}
+
+func (rb *RedBlackBST) moveRedRight(node *Node) *Node {
+	rb.filpColors(node)
+	if rb.isRed(node.left.left) {
+		node = rb.rotateRight(node)
+		rb.filpColors(node)
+	}
+	return node
+}
+
+func (rb *RedBlackBST) balance(h *Node) *Node {
+	if rb.isRed(h.right) {
+		h = rb.rotateLeft(h)
+	}
+	if rb.isRed(h.left) && rb.isRed(h.left.left) {
+		h = rb.rotateRight(h)
+	}
+	if rb.isRed(h.left) && rb.isRed(h.right) {
+		rb.filpColors(h)
+	}
+
+	// h.size = size(h.left) + size(h.right) + 1
 	return h
 }
