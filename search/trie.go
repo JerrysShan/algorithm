00a80a215940d2c30364e141b830882d5e5b3d7b
JerrysShan <1670303003@qq.com>
2017-12-06 23:40:06 +0800

    update

--- search/trie.go
+++ search/trie.go
@@ -3,5 +3,21 @@ package search
 type trieNode struct {
 	data       string
 	freq       int
-	childNodes []trieNode
+	childNodes []*trieNode
+}
+
+func createTrieNode() *trieNode {
+	node := &trieNode{
+		freq:       0,
+		childNodes: make([]*trieNode, 26),
+	}
+	return node
+}
+
+func trieInsert(root *trieNode, key string) {
+
+}
+
+func trieSearch(root *trieNode, key string) {
+
 }
