416b10bd5fb0cb2c1fb9d0aa72aa7710ca00ddba
JerrysShan <1670303003@qq.com>
2017-12-07 23:23:37 +0800

    修改

--- search/trie.go
+++ search/trie.go
@@ -1,23 +1,54 @@
 package search
 
-type trieNode struct {
+type TrieNode struct {
 	data       string
 	freq       int
-	childNodes []*trieNode
+	childNodes []*TrieNode
 }
 
-func createTrieNode() *trieNode {
-	node := &trieNode{
+func createTrieNode() *TrieNode {
+	node := &TrieNode{
 		freq:       0,
-		childNodes: make([]*trieNode, 26),
+		childNodes: make([]*TrieNode, 26),
 	}
 	return node
 }
 
-func trieInsert(root *trieNode, key string) {
-
+func TrieInsert(root *TrieNode, key string) {
+	if len(key) == 0 {
+		return
+	}
+	chars := []byte(key)
+	index := chars[0] - 'a'
+	if root.childNodes[index] == nil {
+		root.childNodes[index] = createTrieNode()
+	}
+	root.childNodes[index].data = key[:1]
+	if len(key) == 1 {
+		root.childNodes[index].freq++
+	} else {
+		TrieInsert(root.childNodes[index], key[1:])
+	}
 }
 
-func trieSearch(root *trieNode, key string) {
+func TrieSearch(root *TrieNode, key string) bool {
+	chars := []byte(key)
+	p := root
+	for _, c := range chars {
+		index := c - 'a'
+		if p.childNodes[index] == nil {
+			return false
+		}
+		p = p.childNodes[index]
+	}
+	return true
+}
 
+func TrieShow(root *TrieNode, str string) {
+	for _, v := range root.childNodes {
+		if v != nil {
+			str = str + "->" + v.data
+			TrieShow(v, str)
+		}
+	}
 }
---
+++ search/trie_test.go
@@ -0,0 +1,27 @@
+package search
+
+import (
+	"fmt"
+	"testing"
+)
+
+var testRoot *TrieNode = createTrieNode()
+
+func TestTrieInsert(t *testing.T) {
+	t.Log("test TrieInsert")
+	TrieInsert(testRoot, "apple")
+	TrieInsert(testRoot, "application")
+	t.Log("should app")
+}
+
+func TestTrieSearch(t *testing.T) {
+	t.Log("test TrieSearch")
+	ret := TrieSearch(testRoot, "applee")
+	fmt.Println(ret, "------------->")
+	if ret {
+		t.Log("'app' find in trieRoot")
+	} else {
+		t.Error("'app' is not founded")
+	}
+	t.Log("ret should be true", ret)
+}
