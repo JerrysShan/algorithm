8fd41a84a03db90b68f66193ccc27670ce3ebd64
shanshengkai <shanshengkai@xiaomi.com>
2019-11-15 09:27:36 +0800

    update max product

--- leetcode/152_max_product.go
+++ leetcode/152_max_product.go
@@ -6,11 +6,14 @@ func maxProduct(nums []int) int {
 	length := len(nums)
 	for i := 0; i < length; i++ {
 		temp := nums[i]
-		for j := i + 1; j < length; j++ {
-			temp *= nums[j]
-		}
 		if temp > max {
 			max = temp
+		}
+		for j := i + 1; j < length; j++ {
+			temp *= nums[j]
+			if temp > max {
+				max = temp
+			}
 		}
 	}
 	return max
--- leetcode/152_max_product_test.go
+++ leetcode/152_max_product_test.go
@@ -4,5 +4,6 @@ import "testing"
 
 func TestMaxProduct(t *testing.T) {
 	t.Log(maxProduct([]int{2, 3, -2, 4}))
-	t.Log(maxProduct([]int{-2, 3, 4}))
+	t.Log(maxProduct([]int{-2, 3, -4}))
+	t.Log(maxProduct([]int{0, 2}))
 }
