1f56149c76af827ccf565762e8ecbbee2b655c3b
shanshengkai <shanshengkai@xiaomi.com>
2019-11-24 21:10:30 +0800

    update

--- leetcode/139_word_break.go
+++ leetcode/139_word_break.go
@@ -5,25 +5,43 @@ func wordBreak(s string, wordDict []string) bool {
 }
 
 func wordBreakFun(s string, wordDict []string, start int, mem []bool) bool {
-	if start == len(s) {
-		return true
-	}
-	for end := start + 1; end <= len(s); end++ {
-		t := s[start:end]
-		if mem[start] {
-			return true
-		}
-		isExist := false
-		for _, v := range wordDict {
-			if v == t {
-				isExist = true
+	// if start == len(s) {
+	// 	return true
+	// }
+	// for end := start + 1; end <= len(s); end++ {
+	// 	t := s[start:end]
+	// 	if mem[start] {
+	// 		return true
+	// 	}
+	// 	isExist := false
+	// 	for _, v := range wordDict {
+	// 		if v == t {
+	// 			isExist = true
+	// 		}
+	// 	}
+	// 	if isExist && wordBreakFun(s, wordDict, end, mem) {
+	// 		mem[start] = true
+	// 		return true
+	// 	}
+	// }
+	// mem[start] = false
+	// return false
+
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			isExist := false
+			for _, v := range wordDict {
+				if v == s[j:i] {
+					isExist = true
+				}
+			}
+			if dp[j] && isExist {
+				dp[i] = true
+				break
 			}
 		}
-		if isExist && wordBreakFun(s, wordDict, end, mem) {
-			mem[start] = true
-			return true
-		}
 	}
-	mem[start] = false
-	return false
+	return dp[len(s)]
 }
--- leetcode/300_length_of_lis_test.go
+++ leetcode/300_length_of_lis_test.go
@@ -3,5 +3,6 @@ package leetcode
 import "testing"
 
 func TestLengthOfLis(t *testing.T) {
-
+	// t.Log(lengthOfLIS([]int{1, 3, 6, 7, 9, 4, 10, 5, 6}))
+	t.Log(lengthOfLIS([]int{11, 12, 13, 14, 15, 6, 7, 8, 101, 18}))
 }
